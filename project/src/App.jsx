ebf46a3cf787f37314febc828cad8ee5fdc801ae
kinginnorthh <neerajravindra10@gmail.com>
2025-07-24 17:04:23 +0530

    add new feature

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
     return (
         <div className = "flex-center h-[100vh]">
             <h1 className = "text-3xl text-red-300">hello  gsap</h1>
+            <h1>hello neeraj</h1>
         </div>
     )
 }
